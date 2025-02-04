274690b5988fc332a5a34ec23bbc3aee10824868
Abraham Paul <codexapp44@gmail.com>
2025-02-04 18:51:14 +0000

    Modify the create method in songs.service to retrieve artist from the db before saving a song

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -4,10 +4,11 @@ import { SongsService } from './songs.service';
 import { connection } from 'src/common/constants/connection';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Song } from './entities/song.entity';
+import { Artist } from 'src/artists/entities/artist.entity';
   
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Song])],
+  imports: [TypeOrmModule.forFeature([Song, Artist])],
   controllers: [SongsController],
   providers: [
     SongsService,
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -2,18 +2,20 @@ import { Injectable, Inject, Scope } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection } from 'src/common/constants/connection';
 import { Song } from './entities/song.entity';
-import { Repository, UpdateResult } from 'typeorm';
+import { In, Repository, UpdateResult } from 'typeorm';
 import { CreateSongDto } from './dto/create-song.dto';
 import { UpdateSongDto } from './dto/update-song.dto';
 import { paginate, Pagination, IPaginationOptions } from 'nestjs-typeorm-paginate';  
+import { Artist } from 'src/artists/entities/artist.entity';
 
 @Injectable({scope: Scope.TRANSIENT})
 export class SongsService {
   constructor(
     @Inject("CONNECTION") connection: Connection,
     @InjectRepository(Song) private songRepository: Repository<Song>,
+    @InjectRepository(Artist) private artistRepository: Repository<Artist>,
   ) {
-    console.log("connection string", connection.CONNECTION_STRING);
+    // console.log("connection string", connection.CONNECTION_STRING);
   }
 
   async paginate(options: IPaginationOptions): Promise<Pagination<Song>> {
@@ -33,6 +35,12 @@ export class SongsService {
     song.duration = songDTO.duration;
     song.lyrics = songDTO.lyrics;
     song.releasedDate = songDTO.releasedDate;
+
+    // fetch the artists from the db
+    // const artists = await this.artistRepository.findByIds(songDTO.artists); // this method is deprecated
+    const artists = await this.artistRepository.findBy({ id: In(songDTO.artists) });
+    song.artists = artists;
+
     return await this.songRepository.save(song);
   }
 
