9287db23e69259d0b227b53c88c78208860900a5
Abraham Paul <codexapp44@gmail.com>
2025-02-04 00:40:04 +0000

    Modify: songs.service, Update: crud methods and add a new delete method. Setup: songRepositry to use for interactions with the database

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 import { connection } from 'src/common/constants/connection';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Song } from './entities/song.entity';
   
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Song])],
   controllers: [SongsController],
   providers: [
     SongsService,
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,21 +1,42 @@
 import { Injectable, Inject, Scope } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Connection } from 'src/common/constants/connection';
+import { Song } from './entities/song.entity';
+import { Repository } from 'typeorm';
+import { CreateSongDto } from './dto/create-song.dto';
 
 @Injectable({scope: Scope.TRANSIENT})
 export class SongsService {
-  constructor(@Inject("CONNECTION") connection: Connection) {
+  constructor(
+    @Inject("CONNECTION") connection: Connection,
+    @InjectRepository(Song) private songRepository: Repository<Song>,
+  ) {
     console.log("connection string", connection.CONNECTION_STRING);
   }
-  // local DB
-  // local array
-  private readonly songs: string[] = [];
-  create(song) {
+
+  async create(songDTO: CreateSongDto): Promise<Song> {
     // Save the song in the database
-    this.songs.push(song);
-    return this.songs;
+    const song = new Song();
+    song.title = songDTO.title;
+    song.artists = songDTO.artists;
+    song.duration = songDTO.duration;
+    song.lyrics = songDTO.lyrics;
+    song.releasedDate = songDTO.releasedDate;
+    return await this.songRepository.save(song);
   }
-  findAll() {
-    // fetch the songs from the db    
-    return this.songs;
+
+  async findAll(): Promise<Song[]> {
+    // fetch the songs from the db
+    return await this.songRepository.find();
+  }
+
+  async findOne(id: number): Promise<Song | null> {
+    // fetch the songs from the db
+    return await this.songRepository.findOneBy({id});
+  }
+
+  async delete(id: number): Promise<void> {
+    // delete song by id
+    await this.songRepository.delete(id);
   }
 }
