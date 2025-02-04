c6f004758d6747064a6de175e93f4a27e3afd3b2
Abraham Paul <codexapp44@gmail.com>
2025-02-04 18:18:18 +0000

    Create: user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
