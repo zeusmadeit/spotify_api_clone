d85c3a1c2123f70982dd9e0a5a05745bbfbba188
Abraham Paul <codexapp44@gmail.com>
2025-02-04 19:08:49 +0000

    Create: create-user.dto and update the create method in users.service

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import {
+  IsArray,
+  IsDate,
+  IsDateString,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsMilitaryTime,
+  IsNotEmpty,
+  IsString,
+} from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly firstName: string;
+
+  @IsString()
+  @IsOptional()
+  readonly lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // Save the user in the database
+    const user = new User();
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+
+    return await this.usersRepository.save(user);
   }
 
   findAll() {
